0d9c53ac9a6ad8d4b4196fc2a4b147a85ac75d1a
Ben Neal <bneal16@gmail.com>
2025-07-01 14:09:41 -0400

    update worlddb25

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='pcm_startlist',
     version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='benvneal',
+    author_email='benvneal@example.com',
     description='A Python package for generating start lists for Pro Cycling Manager from online data.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
